04e4e5d5d9ed9930f89d5faac392e645d9fa5657
SAKSHI333365 <sakshisingh8052863@gmail.com>
2026-06-16 15:12:11 +0530

    Added  Remove Nth Node from End

---
+++ Week_3/Day_2/DeleteNode.java
@@ -0,0 +1,44 @@
+package Week_3.Day_2;
+
+/*
+
+Problem: Detete Nth Node from End
+
+Approach:
+Use two pointers with n nodes gap and remove the the target node.
+
+Time Complexity:O(n)
+Space Complexity:O(1)
+
+*/
+public class DeleteNode {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
