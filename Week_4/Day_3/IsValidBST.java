3f329f610464bee720857772623ad32a85364814
SAKSHI333365 <sakshisingh8052863@gmail.com>
2026-06-24 23:42:13 +0530

    Added IsValid BST solution

---
+++ Week_4/Day_3/IsValidBST.java
@@ -0,0 +1,43 @@
+package Week_4.Day_3;
+
+/*
+
+Problem :Validate Binary Search Tree.
+
+Approach:
+Use  DFS with min and max bounds.
+
+Time Complexity:O(h)
+Space Complexity:O(1)
+
+*/
+
+public class IsValidBST {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode root, long min, long max) {
+
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return validate(root.left, min, root.val) && validate(root.right, root.val, max);
+    }
+}
